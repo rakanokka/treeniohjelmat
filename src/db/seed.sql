00ea46f1ef3149b8269f8546438634478a4a5304
Luukas Niemi <luke.niemi@gmail.com>
2026-09-06 13:45:08 +0300

    Basic sign in/up support

--- src/db/seed.sql
+++ src/db/seed.sql
@@ -1,3 +1,11 @@
 INSERT INTO "user" (username, email, password_hash) VALUES 
-('matti', 'matti@example.com', 'pbkdf2:sha256:...'),
-('teppo', 'teppo@example.com', 'pbkdf2:sha256:...');
+('matti', 'matti@example.com', 'pbkdf2:sha256:260000$bv4m8S28$e66d21392df1df60848ea1110b93836d5952dbd46df3c088b90e932ec13cb897'),
+('teppo', 'teppo@example.com', 'pbkdf2:sha256:260000$bv4m8S28$e66d21392df1df60848ea1110b93836d5952dbd46df3c088b90e932ec13cb897'),
+('liisa', 'liisa@example.com', 'pbkdf2:sha256:260000$bv4m8S28$e66d21392df1df60848ea1110b93836d5952dbd46df3c088b90e932ec13cb897'),
+('kalle', 'kalle@example.com', 'pbkdf2:sha256:260000$bv4m8S28$e66d21392df1df60848ea1110b93836d5952dbd46df3c088b90e932ec13cb897'),
+('anna', 'anna@example.com', 'pbkdf2:sha256:260000$bv4m8S28$e66d21392df1df60848ea1110b93836d5952dbd46df3c088b90e932ec13cb897'),
+('juho', 'juho@example.com', 'pbkdf2:sha256:260000$bv4m8S28$e66d21392df1df60848ea1110b93836d5952dbd46df3c088b90e932ec13cb897'),
+('sara', 'sara@example.com', 'pbkdf2:sha256:260000$bv4m8S28$e66d21392df1df60848ea1110b93836d5952dbd46df3c088b90e932ec13cb897'),
+('mikko', 'mikko@example.com', 'pbkdf2:sha256:260000$bv4m8S28$e66d21392df1df60848ea1110b93836d5952dbd46df3c088b90e932ec13cb897'),
+('emma', 'emma@example.com', 'pbkdf2:sha256:260000$bv4m8S28$e66d21392df1df60848ea1110b93836d5952dbd46df3c088b90e932ec13cb897'),
+('antti', 'antti@example.com', 'pbkdf2:sha256:260000$bv4m8S28$e66d21392df1df60848ea1110b93836d5952dbd46df3c088b90e932ec13cb897');
